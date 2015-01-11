4b6ccfaa16aee4bb033e2ae3d3f6299f9165bb7a
smith-jordan <smith-jordan@users.noreply.github.com>
2015-01-11 10:31:55 -0700

    Create galtolittable.java

---
+++ galtolittable.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package galtolittable.java;
+
+/**
+ *
+ * @author Jordan
+ */
+public class GalToLitTableJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double gallons, liters;
+        int counter;
+        
+        counter = 0;
+        for(gallons = 1; gallons <= 100; gallons++){
+            liters = gallons * 3.7854;
+            System.out.println(gallons + " gallons is " + liters + " liters.");
+            
+            counter++;
+            if(counter==10){
+                System.out.println();
+                counter = 0;
+            }
+        }
+    }
+    
+}
